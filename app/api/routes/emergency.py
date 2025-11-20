6b07e8c11f681d4e5fc4060ccf38f7b3a3d1b782
wole <wole1702@gmail.com>
2025-11-20 03:03:31 +0100

    emergency_contact

--- app/api/routes/emergency.py
+++ app/api/routes/emergency.py
@@ -1,36 +1,50 @@
-# from fastapi import APIRouter, Depends
-# from fastapi.responses import JSONResponse, Response
-# from pydantic import BaseModel
-# from datetime import date, time
-# from app.supabase_client import supabase
-# from app.api.dependencies.auth import get_current_user
-# from typing import List
-# from enum import Enum
+from fastapi import APIRouter, Depends, Request
+from fastapi.responses import JSONResponse, Response
+from pydantic import BaseModel, field_validator
+from app.api.dependencies.auth import get_current_user
+from typing import List
 
 
-# table = supabase.table("emergency_contact")
-
-# router = APIRouter(
-#     prefix="/contact",
-#     tags=["emergencyContact"]
-# )
-
-# class EmergencyContact(BaseModel):
-#     name: str
-#     relationship: str
-#     phone: str
-#     email: str | None=None
 
 
-# @router.post("/")
-# def emergency_contact(contact: EmergencyContact, user=Depends(get_current_user)):
-#     """add emergency contact"""
-#     data = contact.model_dump()
-#     data['user_id'] = user
-#     table.insert(data).execute()
-#     return Response(status_code=201)
+
+router = APIRouter(
+    prefix="/contact",
+    tags=["emergencyContact"]
+)
+
+class EmergencyContact(BaseModel):
+    name: str
+    relationship: str
+    phone: str
+    email: str | None=None
+
+    @field_validator("email", mode="before")
+    def parse_email(cls, v):
+        if v is None:
+            return None
+        if "@" in v:
+            return v
+        try:
+            if "@" not in v:
+              raise ValueError("must a valid email")      
+        except Exception:
+            raise ValueError("must be a valid email")
+
+
+@router.post("/")
+async def emergency_contact(request: Request, contact: EmergencyContact, user=Depends(get_current_user)):
+    """add emergency contact"""
+    supabase = request.app.state.supabase
+    supabase.table("emergency_contacts")
+    data = contact.model_dump()
+    data['user_id'] = user
+    await supabase.table("emergency_contacts").insert(data).execute()
+    return Response(status_code=201)
     
-# @router.get("/")
-# def emergency_contact(user=Depends(get_current_user)):
-#     """get emergency contacts"""
-#     data = table.select("name", "phone", "relationship", "email").eq("user_id", user).execute()
+@router.get("/")
+async def emergency_contact(request: Request, user=Depends(get_current_user)):
+    """get emergency contacts"""
+    supabase = request.app.state.supabase
+    res = await supabase.table("emergency_contacts").select("name", "phone", "relationship", "email").eq("user_id", user).execute()
+    return JSONResponse(status_code=200, content=res.data)
